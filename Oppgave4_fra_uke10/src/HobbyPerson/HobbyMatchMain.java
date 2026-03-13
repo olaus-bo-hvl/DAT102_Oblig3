13a9d16cbd673dcf6bafd93634a741863cd5fe4b
olaus-bo-hvl <olbo04199@hvl.no>
2026-03-13 11:45:51 +0100

    liten start på hobbypersonmain

--- Oppgave4_fra_uke10/src/HobbyPerson/HobbyMatchMain.java
+++ Oppgave4_fra_uke10/src/HobbyPerson/HobbyMatchMain.java
@@ -1,4 +1,14 @@
 package HobbyPerson;
 
 public class HobbyMatchMain {
+
+    public static void main(String[] args) {
+        //Person Endre = new Person("Endre", "Handball", "Trening", "Gaming");
+
+    }
+
+    static double match(Person a, Person b) {
+
+    }
+
 }
