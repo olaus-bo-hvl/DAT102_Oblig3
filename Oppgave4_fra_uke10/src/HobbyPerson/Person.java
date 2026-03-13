b702213ba857de73b6f3ada3bbf11fd16e678a01
endretysnes <entys0795@hvl.no>
2026-03-13 11:47:08 +0100

    1

--- Oppgave4_fra_uke10/src/HobbyPerson/Person.java
+++ Oppgave4_fra_uke10/src/HobbyPerson/Person.java
@@ -1,4 +1,28 @@
 package HobbyPerson;
 
 public class Person {
+    
+    private String navn;
+    private String[] hobbyer;
+
+    public Person(String navn, String ... hobbyer){
+        this.navn = navn;
+        this.hobbyer = hobbyer;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setHobbyer(String[] hobbyer) {
+        this.hobbyer = hobbyer;
+    }
+
+    public String[] getHobbyer() {
+        return hobbyer;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
 }
